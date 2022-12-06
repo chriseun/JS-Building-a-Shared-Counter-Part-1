0f8ce42aeccb0bb5db629d25ca3cd4b25f632334
Chris Eun <eunchristopher@gmail.com>
2022-12-05 19:37:18 -0800

    JS example using JSON

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://localhost:9001/counter')
+    let responseJSON = await response.json()
+    // console.log(responseJSON)
+    let countValue = responseJSON.value;
 
     function increment(){
         countValue++;
